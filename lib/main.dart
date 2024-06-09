b33e34029c276ded121dc3c3db812bcf51440954
Taras Tymchyshyn <tarast@livv.com>
2024-06-09 09:56:39 +0200

    separate main and app class

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/weather_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const WeatherApp());
-}
-
-class WeatherApp extends StatelessWidget {
-  const WeatherApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello Weather!'),
-        ),
-      ),
-    );
-  }
 }
